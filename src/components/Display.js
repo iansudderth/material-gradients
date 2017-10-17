9f5d94597976c22057dc5ffe51bcb34fb5936faf
Your Name <ian.sudderth@gmail.com>
2017-10-16 23:50:18 -0400

    connected gradient

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 const styles = {
   display: {
-    width: "100%",
-    backgroundColor: "red"
+    width: "100%"
   }
 };
 
 const Display = props => {
   const { display } = props.classes;
+  const { startColor, endColor } = props.gradient;
 
-  return <div className={display} />;
+  return (
+    <div
+      className={display}
+      style={{
+        background: `linear-gradient(0deg, ${startColor}, ${endColor})`
+      }}
+    />
+  );
 };
 
-export default withStyles(styles)(Display);
+const mapStateToProps = ({ gradient }) => ({ gradient });
+
+const mapDispatchToProps = dispatch => ({});
+
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(Display)
+);
