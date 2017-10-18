23821199625328ecbfced882e70ccd7e3ada0d58
Your Name <ian.sudderth@gmail.com>
2017-10-17 23:45:08 -0400

    more boilerplate

--- src/components/GradientBlock.js
+++ src/components/GradientBlock.js
@@ -2,13 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-const Display = props => {
+const GradientBlock = props => {
   return <div />;
 };
+
+GradientBlock.propTypes = {};
+
+GradientBlock.defaultProps = {};
 
 const mapStateToProps = ({ reducers }) => ({ reducers });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ actions }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Display);
+export default connect(mapStateToProps, mapDispatchToProps)(GradientBlock);
